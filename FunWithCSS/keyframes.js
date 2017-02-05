8ee209b27c5ee1fad6bef2205c602a2e3339a2a3
Casey McCaskill <caseymccaskill@Caseys-MacBook-Pro.local>
2017-02-05 05:30:31 -0500

    init commit

---
+++ FunWithCSS/keyframes.js
@@ -0,0 +1,15 @@
+
+var panel = '';
+var date = new Date().toLocaleTimeString();
+
+$(document).ready(function(){
+	casey = (()=>{
+			for(var i = 0; i < 120; i ++){
+				panel += '<div class="panel1 col-sm-1">' + date + '<h1>' + i + '</h1>' + '</div>';	
+			}
+		$('.main').html(panel);
+	
+	});
+	setInterval(casey(), 10);
+
+});
