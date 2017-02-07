637737121d8f2ffdd60fb7efcec1dacaa52127b7
Casey McCaskill <caseymccaskill@Caseys-MacBook-Pro.local>
2017-02-06 23:22:02 -0500

    working on react skillz

--- FunWithCSS/keyframes.js
+++ FunWithCSS/keyframes.js
@@ -4,7 +4,7 @@ var date = new Date().toLocaleTimeString();
 
 $(document).ready(function(){
 	casey = (()=>{
-			for(var i = 0; i < 120; i ++){
+			for(var i = 0; i < 121; i ++){
 				panel += '<div class="panel1 col-sm-1">' + date + '<h1>' + i + '</h1>' + '</div>';	
 			}
 		$('.main').html(panel);
